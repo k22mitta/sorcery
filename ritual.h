da03715055976a844db860600537e84c78234294
Bright <168295604+BriteW31@users.noreply.github.com>
2025-07-19 17:59:48 -0400

    Create ritual.h

---
+++ ritual.h
@@ -0,0 +1,22 @@
+#ifndef RITUAL_H
+#define RITUAL_H
+
+#include "card.h"
+
+class Ritual : public Card {
+    int activationCost;
+    int charges;
+
+public:
+    Ritual(const std::string &name, int cost, int activationCost, int charges);
+
+    CardType getType() const override;
+    void display(std::ostream &out) const override;
+    int getActivationCost() const;
+    int getCharges() const;
+    void addCharges(int n);
+    bool canActivate() const;
+    void consumeCharge();
+};
+
+#endif
