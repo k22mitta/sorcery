2646971a9ae29998ea6f98869267bc90344214f0
Bright <168295604+BriteW31@users.noreply.github.com>
2025-07-19 18:28:26 -0400

    Create ability.h

---
+++ ability.h
@@ -0,0 +1,20 @@
+#ifndef ABILITY_H
+#define ABILITY_H
+
+class Game;
+
+class Ability {
+public:
+    virtual void execute(Game &game, int targetPlayer = -1, int targetCard = -1) = 0;
+    virtual ~Ability() = default;
+};
+
+class ActivatedAbility : public Ability {
+    int cost;
+
+public:
+    ActivatedAbility(int cost);
+    int getCost() const;
+};
+
+#endif
