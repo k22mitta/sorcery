9828f3bdfdd33bf4d1e06ccd5770fd51d21fd00f
Bright <168295604+BriteW31@users.noreply.github.com>
2025-07-19 18:14:54 -0400

    Create board.cc

---
+++ board.cc
@@ -0,0 +1,22 @@
+#include "board.h"
+#include <iostream>
+
+Board::Board(std::unique_ptr<Player> p1, std::unique_ptr<Player> p2)
+    : player1{std::move(p1)}, player2{std::move(p2)} {}
+
+Player &Board::getPlayer(int id) {
+    return (id == 1) ? *player1 : *player2;
+}
+
+Player &Board::getOpponent(int id) {
+    return (id == 1) ? *player2 : *player1;
+}
+
+void Board::display() const {
+    // Placeholder for now
+    std::cout << "Displaying board state...\n";
+    std::cout << player1->getName() << " (Life: " << player1->getLife()
+              << ", Magic: " << player1->getMagic() << ")\n";
+    std::cout << player2->getName() << " (Life: " << player2->getLife()
+              << ", Magic: " << player2->getMagic() << ")\n";
+}
