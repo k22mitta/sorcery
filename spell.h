b0a2a617c6d269a6bb907cd9ede87323efb54f0c
Bright <168295604+BriteW31@users.noreply.github.com>
2025-07-19 17:55:16 -0400

    Create spell.h

---
+++ spell.h
@@ -0,0 +1,16 @@
+#ifndef SPELL_H
+#define SPELL_H
+
+#include "card.h"
+
+class Game;
+
+class Spell : public Card {
+public:
+    Spell(const std::string &name, int cost);
+    CardType getType() const override;
+    void display(std::ostream &out) const override;
+    virtual void effect(Game &game, int targetPlayer = -1, int targetCard = -1) = 0;
+};
+
+#endif
