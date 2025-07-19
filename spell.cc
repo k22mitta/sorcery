8d7cd6337831e6592d37a87236d15b90ae3d90c7
Bright <168295604+BriteW31@users.noreply.github.com>
2025-07-19 17:55:37 -0400

    Create spell.cc

---
+++ spell.cc
@@ -0,0 +1,19 @@
+#include "spell.h"
+#include <iostream>
+#include <iomanip>
+
+Spell::Spell(const std::string &name, int cost) : Card{name, cost} {}
+
+CardType Spell::getType() const { return CardType::Spell; }
+
+void Spell::display(std::ostream &out) const {
+    out << "|-------------------------------|\n";
+    out << "| " << std::setw(25) << std::left << name << " | " << std::setw(2) << cost << " |\n";
+    out << "|-------------------------------|\n";
+    out << "| " << std::setw(29) << "Spell" << "|\n";
+    out << "|-------------------------------|\n";
+    out << "| <effect description here>     |\n";
+    out << "|                               |\n";
+    out << "|                               |\n";
+    out << "|-------------------------------|\n";
+}
