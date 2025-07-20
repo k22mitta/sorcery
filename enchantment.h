797c26cb2b9a52c72fa057b1df4fb225f1c150cd
Bright <168295604+BriteW31@users.noreply.github.com>
2025-07-19 20:08:48 -0400

    Update enchantment.h

--- enchantment.h
+++ enchantment.h
@@ -1,12 +1,11 @@
 #ifndef ENCHANTMENT_H
 #define ENCHANTMENT_H
 
-#include "card.h"
 #include "minion.h"
 #include <memory>
 
-class Enchantment : public Card {
-    std::unique_ptr<Card> target;
+class Enchantment : public Minion {
+    std::unique_ptr<Minion> target;
     int attackModifier = 0;
     int defenseModifier = 0;
     bool overrideStats = false;
@@ -14,17 +13,21 @@ class Enchantment : public Card {
     int newDefense = 0;
 
 public:
-    Enchantment(const std::string &name, int cost, std::unique_ptr<Card> target);
+    Enchantment(const std::string &name, int cost, std::unique_ptr<Minion> target);
 
     CardType getType() const override;
     void display(std::ostream &out) const override;
 
-    int getAttack() const;
-    int getDefense() const;
+    int getAttack() const override;
+    int getDefense() const override;
+    void restoreAction() override;
+    bool canAct() const override;
+    void spendAction() override;
+
     void setStatOverride(int atk, int def);
     void setStatModifier(int atkDelta, int defDelta);
 
-    Card &getBase();
+    Minion &getBase();
 };
 
 #endif
