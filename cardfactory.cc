5f5fb15719fd715a7031788ef2bf1576fca538ff
Tony Wang <t85wang@uwaterloo.ca>
2025-07-20 20:37:06 -0400

    changed logic of card generation

--- cardfactory.cc
+++ cardfactory.cc
@@ -18,6 +18,7 @@ std::vector<std::unique_ptr<Card>> CardFactory::loadDeck(const std::string &file
 }
 
 std::unique_ptr<Card> CardFactory::createCard(const std::string &name) {
+    /*
     if (name == "Novice Pyromancer") {
         return std::make_unique<Minion>("Novice Pyromancer", 1, 0, 1);
     } else if (name == "Air Elemental") {
@@ -45,7 +46,18 @@ std::unique_ptr<Card> CardFactory::createCard(const std::string &name) {
     } else if (name == "Dark Ritual") {
         return std::make_unique<Ritual>("Dark Ritual", 0, 1, 5);
     }
-
-    std::cerr << "Unknown card name: " << name << std::endl;
-    return nullptr;
+    */
+    if (name == "Dark Ritual") return std::make_unique<DarkRitual>();
+    else if (name == "Aura of Power") return std::make_unique<AuraOfPower>();
+    else if (name == "Standstill") return std::make_unique<StandStill>();
+    else if (name == "Banish") return std::make_unique<Banish>();
+    else if (name == "Unsummon") return std::make_unique<Unsummon>();
+    else if (name == "Recharge") return std::make_unique<Recharge>();
+    else if (name == "Disenchant") return std::make_unique<Disenchant>();
+    else if (name == "RaiseDead") return std::make_unique<RaiseDead>();
+    else if (name == "Blizzard") return std::make_unique<Blizzard>();
+    else {
+        std::cerr << "Unknown card name: " << name << std::endl;
+        return nullptr;
+    }
 }
