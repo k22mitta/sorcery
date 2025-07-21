bbef383707cbb6e883d6874160f15e8498f54a4c
Tony Wang <t85wang@uwaterloo.ca>
2025-07-21 09:44:54 -0400

    basic implementation of 8 minions

--- cardfactory.cc
+++ cardfactory.cc
@@ -18,36 +18,15 @@ std::vector<std::unique_ptr<Card>> CardFactory::loadDeck(const std::string &file
 }
 
 std::unique_ptr<Card> CardFactory::createCard(const std::string &name) {
-    /*
-    if (name == "Novice Pyromancer") {
-        return std::make_unique<Minion>("Novice Pyromancer", 1, 0, 1);
-    } else if (name == "Air Elemental") {
-        return std::make_unique<Minion>("Air Elemental", 0, 1, 1);
-    } else if (name == "Blizzard") {
-        class Blizzard : public Spell {
-        public:
-            Blizzard() : Spell("Blizzard", 3) {}
-            void effect(Game &game, int, int) override {
-                std::cout << "Blizzard deals 2 damage to all minions.\n";
-                // TODO: Apply effect to board
-            }
-        };
-        return std::make_unique<Blizzard>();
-    } else if (name == "Giant Strength") {
-        class GiantStrength : public Enchantment {
-        public:
-            GiantStrength(std::unique_ptr<Card> target)
-                : Enchantment("Giant Strength", 1, std::move(target)) {
-                setStatModifier(2, 2);
-            }
-        };
-        // Return a placeholder target for now
-        return std::make_unique<GiantStrength>(std::make_unique<Minion>("Dummy", 0, 1, 1));
-    } else if (name == "Dark Ritual") {
-        return std::make_unique<Ritual>("Dark Ritual", 0, 1, 5);
-    }
-    */
-    if (name == "Dark Ritual") return std::make_unique<DarkRitual>();
+    if (name == "Air Elemental") return std::make_unique<Minion>("Air Elemental", 0, 1, 1);
+    else if (name == "Earth Elemental") return std::make_unique<Minion>("Earth Elemental", 3, 4, 4);
+    else if (name == "Bone Golem") return std::make_unique<Minion>("Bone Golem", 2, 1, 3);
+    else if (name == "Fire Elemental") return std::make_unique<Minion>("Fire Elemental", 2, 2, 2);
+    else if (name == "Potion Seller") return std::make_unique<Minion>("Potion Seller", 2, 1, 3);
+    else if (name == "Novice Pyromancer") return std::make_unique<Minion>("Novice Pyromancer", 1, 0, 1);
+    else if (name == "Apprentice Summoner") return std::make_unique<Minion>("Apprentice Summoner", 1, 1, 1);
+    else if (name == "Master Summoner") return std::make_unique<Minion>("Master Summoner", 3, 2, 3);
+    else if (name == "Dark Ritual") return std::make_unique<DarkRitual>();
     else if (name == "Aura of Power") return std::make_unique<AuraOfPower>();
     else if (name == "Standstill") return std::make_unique<StandStill>();
     else if (name == "Banish") return std::make_unique<Banish>();
