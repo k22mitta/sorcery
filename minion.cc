92967dacc07c4610c0d481967b7a0eebc3ef9005
Tony Wang <t85wang@uwaterloo.ca>
2025-07-22 15:54:07 -0400

    Revert "Update minion.cc"
    This reverts commit 8f3df7af9603af956b4d4eec05ff6a2f6c93dd52.

--- minion.cc
+++ minion.cc
@@ -3,23 +3,33 @@
 #include <iomanip>
 
 Minion::Minion(const std::string &name, int cost, int atk, int def)
-    : Card{name, cost}, atk{atk}, def{def} {}
+    : Card{name, cost}, atk{atk}, def{def}, actions{0} {}
 
-CardType Minion::getType() const {
-    return CardType::Minion;
-}
+CardType Minion::getType() const { return CardType::Minion; }
 
 void Minion::display(std::ostream &out) const {
-    out << std::left << std::setw(20) << name << " | Cost: " << cost << " | ATK: " << atk << " | DEF: " << def;
-    if (ability) out << " | Ability Cost: " << dynamic_cast<ActivatedAbility *>(ability.get())->getCost();
-    out << std::endl;
+    out << "|-------------------------------|\n";
+    out << "| " << std::setw(25) << std::left << name << " | " << std::setw(2) << cost << " |\n";
+    out << "|-------------------------------|\n";
+    out << "| " << std::setw(29) << "Minion" << "|\n";
+    out << "|-------------------------------|\n";
+    out << "|                               |\n";
+    out << "|                               |\n";
+    out << "|                               |\n";
+    out << "|------                   ------|\n";
+    out << "| " << std::setw(3) << atk << " |                   | " << std::setw(3) << def << " |\n";
+    out << "|-------------------------------|\n";
 }
 
 int Minion::getAttack() const { return atk; }
 int Minion::getDefense() const { return def; }
+void Minion::setAttack(int val) { atk = val; }
+void Minion::setDefense(int val) { def = val; }
+void Minion::modifyStats(int atkDelta, int defDelta) { atk += atkDelta; def += defDelta; }
+
 bool Minion::canAct() const { return actions > 0; }
 void Minion::restoreAction() { actions = 1; }
 void Minion::spendAction() { if (actions > 0) --actions; }
 
-void Minion::setAbility(std::unique_ptr<Ability> a) { ability = std::move(a); }
-Ability *Minion::getAbility() const { return ability.get(); }
+Ability* Minion::getAbility() { return ability.get(); }
+void Minion::setAbility(std::unique_ptr<Ability> ability) { this -> ability = std::move(ability); }
