6e4768d4bb3aaace63418dda28870f599e42b072
Bright <168295604+BriteW31@users.noreply.github.com>
2025-07-24 19:16:59 -0400

    Update minion.cc

--- minion.cc
+++ minion.cc
@@ -70,21 +70,11 @@ card_template_t Minion::display() const {
     return display_minion_no_ability(name, cost, atk, def);
 }
 
+/*
 BoneGolem::BoneGolem(): Minion{"Bone Golem", 2, "Gain +1/+1 whenever a minion leaves play.", 1, 3} {}
 FireElemental::FireElemental(): Minion{"Fire Elemental", 2, "Whenever an opponent's minion enters play, deal 1 damage to it.", 2, 2} {}
 PotionSeller::PotionSeller(): Minion{"Potion Seller", 2, "At the end of your turn, all your minions gain +0/+1.", 1, 3} {}
 NovicePyromancer::NovicePyromancer(): Minion{"Novice Pyromancer", 1, "Deal 1 damage to target minion", 0, 1} {}
 ApprenticeSummoner::ApprenticeSummoner(): Minion{"Apprentice Summoner", 1, "Summon a 1/1 air elemental", 1, 1} {}
 MasterSummoner::MasterSummoner(): Minion{"Master Summoner", 3, "Summon up to three 1/1 air elementals", 2, 3} {}
-
-
-card_template_t Minion::display() const {
-    /*
-    if (ability) {
-        return display_minion_activated_ability(name, cost, atk, def, abilityCost, abilityDesc);
-    } else {
-        return display_minion_no_ability(name, cost, atk, def);
-    }
-    */
-    return display_minion_no_ability(name, cost, atk, def);
-}
+*/
