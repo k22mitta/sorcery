115153d896b78a0478f9507cfda3a4cd4117503c
khush <139495074+k22mitta@users.noreply.github.com>
2025-07-25 05:49:51 +0530

    Update board.h

--- board.h
+++ board.h
@@ -3,12 +3,23 @@
 
 #include <memory>
 #include "player.h"
+#include "ability.h"
+#include <vector>
+#include <string>
 
 #define BLOCK_HEIGHT 11
+struct Listener {
+        std::string key;
+        Ability * cb;
+};
+
+class Game;
 
 class Board {
     std::unique_ptr<Player> player1;
     std::unique_ptr<Player> player2;
+    Game *game;
+    std::vector<Listener> listeners;
     void print(const std::vector<card_template_t> &cards, int height) const;
 
 public:
@@ -18,6 +29,12 @@ public:
     Player &getOpponent(int id);
     void display() const;
     void displayHand(Player& player) const;
+    void registerListener(const std::string& key, Ability* ability);
+    void notify(const std::string& key, int playerId = -1, int boardIdx = -1);
+    void start_turn();
+    void end_turn();
+    void minion_enter(int p,int i);
+    void minion_leave(int p,int i);
 };
 
 #endif
